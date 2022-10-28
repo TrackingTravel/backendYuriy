ba6bb0b2c5eb2e9a2a3a3507c1af550bcbd72d73
bones <j-thrash@ya.ru>
2022-10-28 16:34:41 +0300

    Initial commit

---
+++ src/main/java/backend/tracking_travel/TrackingTravelApplication.java
@@ -0,0 +1,13 @@
+package backend.tracking_travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/tracking_travel/TrackingTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.tracking_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
